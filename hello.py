87863df23d8f95dd53d6cb1c14d05c044597a5d4
Shriganesh <ganeshkolhe4392@gmail.com>
2019-03-16 17:37:12 -0600

    first-module-dummy

---
+++ hello.py
@@ -0,0 +1,14 @@
+def world():
+    print("Hello, World!")
+
+shark = "Sammy"
+
+class Octopus:
+    def __init__(self,name,color):
+        self.color = color
+        self.name = name
+        
+    def tell_more(self):
+        print("this Octopus is "+ self.color  + ".")
+        print(self.name + " is octopus's name.")
+        
---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 16 17:18:19 2019
+
+@author: shriganeshkolhe
+"""
+
+import hello
+
+hello.world()
+
+print(hello.shark)
+
+j = hello.Octopus("jesse","orange")
+
